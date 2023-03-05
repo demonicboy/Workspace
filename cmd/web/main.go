46f785b78f58f5cc640aebf0230f03e4e0844285
Toàn Đinh Quốc <quoctoan.tdt@gmail.com>
2023-03-05 16:04:44 +0700

    lecture 35

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,13 +12,21 @@ const portNumber = ":8080"
 
 func main() {
 	var app config.AppConfig
+
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("Cannot create template cache")
 	}
+
 	app.TemplateCache = tc
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
 
 	_ = http.ListenAndServe(portNumber, nil)
 }
--- pkg/handlers/handler.go
+++ pkg/handlers/handler.go
@@ -2,14 +2,35 @@ package handlers
 
 import (
 	"net/http"
+	"workspace/pkg/config"
 	"workspace/pkg/render"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
+// the repository used by the handlers
+var Repo *Repository
+
+// the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// create a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintf(w, "This is the Home page")
 	render.RenderTemplate(w, "home.page.tmpl")
 }
-func About(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintf(w, "This is the About page")
 	render.RenderTemplate(w, "about.page.tmpl")
 }
